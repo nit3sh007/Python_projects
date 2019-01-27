d67ce57053e75a7e35e164a502d134e1ba9ea162
Nitesh kumar <30889903+smartnitesh@users.noreply.github.com>
2019-01-27 15:15:36 +0530

    Add files via upload
    Prime Number

---
+++ prime.py
@@ -0,0 +1,15 @@
+#dafault value
+num = 45
+
+#taking input
+#num = int(input("enter your number:"))
+if num > 1:
+    for i in range(2,num):
+        if(num % i) == 0:
+            print(num,"is not a prime number")
+            break
+        else:
+            print(num,"is a prime num")
+            break
+else:
+        print(num,"is not a prime num")
